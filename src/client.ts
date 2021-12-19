d96d50b5ce78973c6a5d8f86ed37062547b50c15
Winrey <i@iaside.com>
2021-12-19 20:28:32 +0800

    0.2.2

--- src/client.ts
+++ src/client.ts
@@ -1,88 +1,80 @@
-import { IClient, IReturn, TOptions } from "@embassy/interface";
-import { FC, FCClient, IFCClientConfig, IServiceResponse } from "@winry/fc2";
+import { IClient, IReturn, TOptions } from '@embassy/interface';
+import { FC, FCClient, IFCClientConfig, IServiceResponse } from '@winry/fc2';
 import { AliConnector } from './connector';
 
-
 export type TClientOptions = {
-  accountId: string,
-} & IFCClientConfig
-
+  accountId: string;
+} & IFCClientConfig;
 
 export class AliClient implements IClient {
-  client: FCClient
+  client: FCClient;
 
   connector = new AliConnector();
 
-  constructor(config: TClientOptions){
-    const { accountId, ...rest } = config
-    this.client = new FCClient(accountId, rest)
+  constructor(config: TClientOptions) {
+    const { accountId, ...rest } = config;
+    this.client = new FCClient(accountId, rest);
   }
 
-  async invoke<
-    TEvent = string | object, 
-    TResult = void
-  >(
-    srvName: string, funcName: string, 
-    event?: TEvent, options: TOptions = {},
+  async invoke<TEvent = string | object, TResult = void>(
+    srvName: string,
+    funcName: string,
+    event?: TEvent,
+    options: TOptions = {},
   ): Promise<AliReturn<TResult>> {
-    const { headers, qualifier, ...rest } = options
-    let args
+    const { headers, qualifier, ...rest } = options;
+    let args;
     if (typeof event === 'string' || event instanceof Buffer) {
-      args = event
+      args = event;
     } else if (event) {
-      args = Buffer.from(JSON.stringify(event))
+      args = Buffer.from(JSON.stringify(event));
     }
-    const raw = await this.client.invokeFunction(
-      srvName, funcName, args, 
-      headers, qualifier, rest
-    )
-    const re = new AliReturn<TResult>(raw)
-    return re
+    const raw = await this.client.invokeFunction(srvName, funcName, args, headers, qualifier, rest);
+    const re = new AliReturn<TResult>(raw);
+    return re;
   }
-
 }
 
 export class AliReturn<T = void> implements IReturn<T> {
-  raw: IServiceResponse
+  raw: IServiceResponse;
 
   constructor(raw: IServiceResponse) {
-    this.raw = raw
+    this.raw = raw;
   }
   getData(): T {
-    return this.raw.data as unknown as T
+    return this.raw.data as unknown as T;
   }
   getHeaders(): object {
-    return this.raw.headers
+    return this.raw.headers;
   }
   toBuffer(): Buffer {
-    const { data } = this.raw
+    const { data } = this.raw;
     if (data instanceof Buffer) {
-      return data
+      return data;
     }
-    if (typeof data === "string") {
-      return Buffer.from(data)
+    if (typeof data === 'string') {
+      return Buffer.from(data);
     }
-    return Buffer.from(JSON.stringify(data))
+    return Buffer.from(JSON.stringify(data));
   }
   toString(): string {
-    const { data } = this.raw
+    const { data } = this.raw;
     if (data instanceof Buffer) {
-      return data.toString()
+      return data.toString();
     }
-    if (typeof data === "string") {
-      return data
+    if (typeof data === 'string') {
+      return data;
     }
-    return JSON.stringify(data)
+    return JSON.stringify(data);
   }
   toJson(): object {
-    let { data } = this.raw
+    let { data } = this.raw;
     if (data instanceof Buffer) {
-      data = data.toString()
+      data = data.toString();
     }
-    if (typeof data === "string") {
-      data = JSON.parse(data)
+    if (typeof data === 'string') {
+      data = JSON.parse(data);
     }
-    return data as object
+    return data as object;
   }
-
 }
